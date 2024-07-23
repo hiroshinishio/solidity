67e4a37553f0460c477a549d7d9473d080dfdc0c
Matheus Aguiar <matheus.pit@gmail.com>
2024-07-23 14:56:59 -0300

    Add test

---
+++ test/libsolidity/semanticTests/variables/transient_state_variables_init_order.sol
@@ -0,0 +1,24 @@
+contract A {
+    uint transient public x = 0;
+    uint transient y = f();
+    function f() public returns (uint) {
+        ++x;
+        return 42;
+    }
+}
+contract B is A {
+    uint transient public z;
+    constructor() {
+        z = x;
+    }
+    function getY() public returns (uint) {
+        return y;
+    }
+ }
+// ====
+// compileViaYul: false
+// ----
+// x() -> 0
+// z() -> 0
+// getY() -> 0
+
